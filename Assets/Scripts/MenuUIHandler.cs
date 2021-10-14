34e07b9cf14dfde163d3b6ffee1849227a1f5b1a
Irishallo <68908273+Irishallo@users.noreply.github.com>
2021-10-14 14:10:36 +0200

    got the menu to work
    you can start the game and type a name that the mainmenumanager script will save

---
+++ Assets/Scripts/MenuUIHandler.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuUIHandler : MonoBehaviour
+{
+    public InputField mainInputField;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartNew()
+    {
+        MainMenuManager.Instance.playerName = mainInputField.text;
+        SceneManager.LoadScene(1);
+    }
+}
